5292c352c784d7f5190dcb4b8e4113a774b06beb
jeffer-coder <jefersoncoder@gmail.com>
2022-01-10 22:02:06 -0300

    morgan and auth

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,12 @@ import handling from '@app/middlewares/handling'
 import router from '@app/router'
 import '@db/db'
 import express from 'express'
+import morgan from 'morgan'
+import cors from 'cors'
 const app = express()
 
-
+app.use(cors({origin:'*'}))
+app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
